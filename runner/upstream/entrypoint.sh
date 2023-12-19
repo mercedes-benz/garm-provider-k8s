01e0224bd67965ec4ca0bbde64d9ee6300c10ff4
Mario Constanti <mario.constanti@mercedes-benz.com>
2023-12-19 06:59:50 +0100

    fix: add missing license header
    Signed-off-by: Mario Constanti <mario.constanti@mercedes-benz.com>

--- runner/upstream/entrypoint.sh
+++ runner/upstream/entrypoint.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
-
-#!/bin/bash
+# SPDX-License-Identifier: MIT
 
 set -ex
 set -o pipefail
 
-if [ -z "$METADATA_URL" ];then
-	echo "no token is available and METADATA_URL is not set"
-	exit 1
+if [ -z "$METADATA_URL" ]; then
+    echo "no token is available and METADATA_URL is not set"
+    exit 1
 fi
 
-if [ -z "$CALLBACK_URL" ];then
-	echo "CALLBACK_URL is not set"
-	exit 1
+if [ -z "$CALLBACK_URL" ]; then
+    echo "CALLBACK_URL is not set"
+    exit 1
 fi
 
 function call() {
-	PAYLOAD="$1"
-	[[ $CALLBACK_URL =~ ^(.*)/status(/)?$ ]] || CALLBACK_URL="${CALLBACK_URL}/status"
-	curl --retry 5 --retry-delay 5 --retry-connrefused --fail -s -X POST -d "${PAYLOAD}" -H 'Accept: application/json' -H "Authorization: Bearer ${BEARER_TOKEN}" "${CALLBACK_URL}" || echo "failed to call home: exit code ($?)"
+    PAYLOAD="$1"
+    [[ $CALLBACK_URL =~ ^(.*)/status(/)?$ ]] || CALLBACK_URL="${CALLBACK_URL}/status"
+    curl --retry 5 --retry-delay 5 --retry-connrefused --fail -s -X POST -d "${PAYLOAD}" -H 'Accept: application/json' -H "Authorization: Bearer ${BEARER_TOKEN}" "${CALLBACK_URL}" || echo "failed to call home: exit code ($?)"
 }
 
 function sendStatus() {
-	MSG="$1"
-	call "{\"status\": \"installing\", \"message\": \"$MSG\"}"
+    MSG="$1"
+    call "{\"status\": \"installing\", \"message\": \"$MSG\"}"
 }
 
 function fail() {
-	MSG="$1"
-	call "{\"status\": \"failed\", \"message\": \"$MSG\"}"
-	exit 1
+    MSG="$1"
+    call "{\"status\": \"failed\", \"message\": \"$MSG\"}"
+    exit 1
 }
 
 function success() {
-	MSG="$1"
-	ID=$2
+    MSG="$1"
+    ID=$2
     if [ $JIT_CONFIG_ENABLED != "true" ] && [ -z "$ID" ]; then
         fail "agent ID is required when JIT_CONFIG_ENABLED is not true"
     fi
@@ -42,15 +41,15 @@ function success() {
     if [ -z "$ID" ]; then
         ID="null"
     fi
-	call "{\"status\": \"idle\", \"message\": \"$MSG\", \"agent_id\": $ID}"
+    call "{\"status\": \"idle\", \"message\": \"$MSG\", \"agent_id\": $ID}"
 }
 
 function getRunnerFile() {
-	curl --retry 5 --retry-delay 5 \
-		--retry-connrefused --fail -s \
-		-X GET -H 'Accept: application/json' \
-		-H "Authorization: Bearer ${BEARER_TOKEN}" \
-		"${METADATA_URL}/$1" -o "$2"
+    curl --retry 5 --retry-delay 5 \
+        --retry-connrefused --fail -s \
+        -X GET -H 'Accept: application/json' \
+        -H "Authorization: Bearer ${BEARER_TOKEN}" \
+        "${METADATA_URL}/$1" -o "$2"
 }
 
 pushd /home/runner/
@@ -82,7 +81,7 @@ else
     attempt=1
     while true; do
         ERROUT=$(mktemp)
-        if [ ! -z $RUNNER_GROUP ] && [ $RUNNER_GROUP != "default" ];then
+        if [ ! -z $RUNNER_GROUP ] && [ $RUNNER_GROUP != "default" ]; then
             ./config.sh --unattended \
                 --url "$REPO_URL" \
                 --token "$GITHUB_TOKEN" \
@@ -110,13 +109,13 @@ else
         # managed to register it but timed out later, resulting in an error.
         ./config.sh remove --token "$GITHUB_TOKEN" || true
 
-        if [ $attempt -gt 5 ];then
+        if [ $attempt -gt 5 ]; then
             rm $ERROUT || true
             fail "failed to configure runner: $LAST_ERR"
         fi
 
         sendStatus "failed to configure runner (attempt $attempt): $LAST_ERR (retrying in 5 seconds)"
-        attempt=$((attempt+1))
+        attempt=$((attempt + 1))
         rm $ERROUT || true
         sleep 5
     done
@@ -127,8 +126,8 @@ AGENT_ID=""
 
 if [ $JIT_CONFIG_ENABLED != "true" ]; then
     set +e
-    AGENT_ID=$(grep "agentId" /home/runner/.runner |  tr -d -c 0-9)
-    if [ $? -ne 0 ];then
+    AGENT_ID=$(grep "agentId" /home/runner/.runner | tr -d -c 0-9)
+    if [ $? -ne 0 ]; then
         fail "failed to get agent ID"
     fi
     set -e
